84b8b2bfedb2f10a942d0e15f7ca16a812ea7f4e
Mike Hartley <mikehartley@users.noreply.github.com>
2014-10-01 22:50:09 +0100

    Got login forms working, and conditional on role tag also working.

--- src/main/java/uk/co/yottr/config/AppConfig.java
+++ src/main/java/uk/co/yottr/config/AppConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -44,9 +46,9 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return messageSource;
     }
 
-//    @Override
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/login2").setViewName("login2");
-//        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/signin*").setViewName("signin");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
  }
